37f9c90e623d9afe0111d24dadd2ae172b61f0cf
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-10-18 09:40:13 +0300

    Initial commit

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const app = express();
 app.use(express.json()); // parse json bodies in the request object
 
 // Redirect requests to endpoint starting with /posts to postRoutes.js
-// app.use("/posts", require("./routes/postRoutes"));
+app.use("/posts", require("./routes/postRoutes"));
 
 // Global Error Handler. IMPORTANT function params MUST start with err
 app.use((err, req, res, next) => {
-  console.log(err.stack);
-  console.log(err.name);
-  console.log(err.code);
+    console.log(err.stack);
+    console.log(err.name);
+    console.log(err.code);
 
-  res.status(500).json({
-    message: "Something went rely wrong",
-  });
+    res.status(500).json({
+        message: "Something went rely wrong",
+    });
 });
 
 // Listen on pc port
