3ae709a02ca355b93d3af50311e9cf65802daade
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-10-18 11:39:26 +0300

    Make query to database

--- config/db.js
+++ config/db.js
@@ -1,13 +1,25 @@
-const mysql = require('mysql');
-const db_con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "606969",
-    database: "node_express_starter"
+require("dotenv").config();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
 });
-db_con.connect(function(err) {
-    if (!err) {
-        console.log("connected");
-    } else
-        console.log(err.message)
-});
+
+
+module.exports = pool.promise();
+
+// const db_con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "606969",
+//     database: "node_express_starter"
+// });
+// db_con.connect(function(err) {
+//     if (!err) {
+//         console.log("connected");
+//     } else
+//         console.log(err.message)
+// });
